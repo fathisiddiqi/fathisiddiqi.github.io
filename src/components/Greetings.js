f668926d5366de3ef7b00e5a84b31e48603c0c9a
Muhammad Fathi <33077644+fathisiddiqi@users.noreply.github.com>
2022-06-01 09:04:31 +0700

    Update Greetings.js

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -11,8 +11,7 @@ const Greetings = () => {
             strings: [
               "<span style='color: crimson'>I'm Muhammad Fathi</span>",
               "<span style='color: coral'>I'm Software Engineer</span>",
-              "<span style='color: coral'>I'm Technopreneur</span>",
-              "<span style='color: olive'>I'm MERN Stack Developer</span>",
+              "<span style='color: coral'>I'm Backend Engineer</span>",
               "<span style='color: cornflowerblue'>I'm Freelancer</span>",
             ],
             autoStart: true,
